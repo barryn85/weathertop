d8e41a3815ee763bc4ac73e60569a7bb9f97bc78
Barry Nolan <182120232+barryn85@users.noreply.github.com>
2025-10-20 10:01:18 +0100

    Refactor addReport to include additional fields

--- controllers/report-controller.js
+++ controllers/report-controller.js
@@ -2,13 +2,18 @@ import { reportStore } from "../models/report-store.js";
 
 export const reportController = {
   async addReport(request, response) {
-    const newReport = {
-      title: request.body.title,
-      content: request.body.content,
+    const stationId = request.params.id;
+    // expects a simple form with these names
+    const report = {
+      code: request.body.code,
+      temperature: Number(request.body.temperature),
+      windSpeed: Number(request.body.windSpeed),
+      windDirection: Number(request.body.windDirection),
+      pressure: Number(request.body.pressure),
     };
-    console.log(`Adding report ${newReport.title}`);
-    await reportStore.addReport(newReport);
-    response.redirect("/station/" + request.params.id);
+    console.log(`Adding report to station ${stationId}`);
+    await reportStore.addReport(stationId, report);
+    response.redirect("/station/" + stationId);
   },
 
   async deleteReport(request, response) {
@@ -16,6 +21,4 @@ export const reportController = {
     await reportStore.deleteReport(request.params.reportid);
     response.redirect("/station/" + request.params.id);
   },
-
-  
 };
